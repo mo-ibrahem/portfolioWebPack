ba33343954a0fb064afe6d51d9fd82cbb4df9be5
Mohamed Abdelaziz <mmody.zizo@Hotmail.com>
2022-11-06 14:53:47 +0000

    finished the responsive design

--- app/index.js
+++ app/index.js
@@ -1,5 +1,22 @@
-console.log('Hello, world')
-import img from './images/placeholder.png'
+import 'pages/about'
+import 'pages/detail'
+import 'pages/collections'
+import 'pages/home'
+class App{
+  constructor(){
+    console.log('app constructor call')
+  }
+  createPages(){
+    this.pages = {
+      home: new Home(),
+      collection: new Collection(),
+      detail: new Detail(),
+      about: new About()
+    }
+  }
+}
+
+new App()
 
 
 
